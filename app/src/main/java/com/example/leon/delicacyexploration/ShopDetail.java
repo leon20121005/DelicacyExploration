801d10e3005e8143070b824b1f79dd5fe9ed6743
leon20121005 <leon20121005@gmail.com>
2017-08-09 00:15:23 +0800

    create google maps in shop detail

--- app/src/main/java/com/example/leon/delicacyexploration/ShopDetail.java
+++ app/src/main/java/com/example/leon/delicacyexploration/ShopDetail.java
@@ -5,14 +5,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.annotation.Nullable;
 import android.widget.TextView;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 //Created by leon on 2017/8/6.
 
-public class ShopDetail extends Fragment
+public class ShopDetail extends Fragment implements OnMapReadyCallback
 {
     private Shop _data;
+    private GoogleMap _googleMap;
+
 
     @Nullable
     @Override
@@ -36,6 +42,45 @@ public class ShopDetail extends Fragment
         viewHolder.shopDetailName.setText(_data.GetName());
         viewHolder.shopDetailEvaluation.setText(_data.GetEvaluation());
         viewHolder.shopDetailAddress.setText(_data.GetAddress());
+
+        viewHolder.mapView = (MapView) view.findViewById(R.id.shopDetailMapView);
+        viewHolder.mapView.onCreate(savedInstanceState);
+        viewHolder.mapView.onResume();
+        viewHolder.mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        _googleMap = googleMap;
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        (new ViewHolder()).mapView.onResume();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        (new ViewHolder()).mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        (new ViewHolder()).mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        (new ViewHolder()).mapView.onLowMemory();
     }
 
     public void SetShopData(Shop data)
@@ -48,5 +93,6 @@ public class ShopDetail extends Fragment
         TextView shopDetailName;
         TextView shopDetailEvaluation;
         TextView shopDetailAddress;
+        MapView mapView;
     }
 }
