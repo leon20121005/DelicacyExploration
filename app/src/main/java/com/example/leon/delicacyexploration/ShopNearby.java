759ae675a9eb52cd1557186f7dc19b23512618da
leon20121005 <leon20121005@gmail.com>
2017-09-03 01:42:37 +0800

    create spinner to select the query range

--- app/src/main/java/com/example/leon/delicacyexploration/ShopNearby.java
+++ app/src/main/java/com/example/leon/delicacyexploration/ShopNearby.java
@@ -13,7 +13,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 import android.support.annotation.Nullable;
 
@@ -28,6 +30,9 @@ public class ShopNearby extends Fragment implements AsyncResponse
 
     private double _latitude;
     private double _longitude;
+
+    private final double DEFAULT_QUERY_RANGE = 3;
+    private double _queryRange = DEFAULT_QUERY_RANGE;
 
     @Nullable
     @Override
@@ -49,11 +54,7 @@ public class ShopNearby extends Fragment implements AsyncResponse
         returnButton.hide();
 
         InitializeLocation();
-
-        String latitude = "lat=" + Double.toString(_latitude);
-        String longitude = "lng=" + Double.toString(_longitude);
-        String queryURL = getString(R.string.server_ip_address) + NEARBY_SHOP_URL + "?" + latitude + "&" + longitude + "&radius=5&limit=20";
-        new HttpRequestAsyncTask((Fragment) this).execute(queryURL);
+        InitializeSpinner(view);
     }
 
     @Override
@@ -70,13 +71,24 @@ public class ShopNearby extends Fragment implements AsyncResponse
         LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
         if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
         {
-            Location myLocation = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-            if (myLocation != null)
+            Location currentLocation = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+            if (currentLocation != null)
             {
-                _latitude = myLocation.getLatitude();
-                _longitude = myLocation.getLongitude();
+                _latitude = currentLocation.getLatitude();
+                _longitude = currentLocation.getLongitude();
             }
         }
+    }
+
+    private void SendQuery()
+    {
+        String latitude = "lat=" + Double.toString(_latitude);
+        String longitude = "lng=" + Double.toString(_longitude);
+        String radius = "radius=" + Double.toString(_queryRange);
+        String limit = "limit=" + Integer.toString(20);
+        String queryURL = getString(R.string.server_ip_address) + NEARBY_SHOP_URL + "?" + latitude + "&" + longitude + "&" + radius + "&" + limit;
+
+        new HttpRequestAsyncTask((Fragment) this).execute(queryURL);
     }
 
     //初始化ListView
@@ -95,7 +107,58 @@ public class ShopNearby extends Fragment implements AsyncResponse
             }
         });
 
-        Toast.makeText(getActivity(), "顯示附近5公里店家", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "顯示附近" + Double.toString(_queryRange) + "公里店家", Toast.LENGTH_SHORT).show();
         Toast.makeText(getActivity(), Double.toString(_latitude) + ", " + Double.toString(_longitude), Toast.LENGTH_SHORT).show();
+    }
+
+    //初始化Spinner
+    private void InitializeSpinner(View view)
+    {
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        String[] range = {"200公尺", "500公尺", "1公里", "3公里", "5公里", "10公里"};
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, range);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerArrayAdapter);
+
+        spinner.setSelection(3, false); //第二個參數為false讓這個設定不會實際觸發onItemSelected()
+        if (_queryRange == DEFAULT_QUERY_RANGE) //只有在預設範圍 (初始化完)時才主動送出Query
+        {
+            SendQuery();
+        }
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                switch (position)
+                {
+                    case 0:
+                        _queryRange = 0.2;
+                        break;
+                    case 1:
+                        _queryRange = 0.5;
+                        break;
+                    case 2:
+                        _queryRange = 1;
+                        break;
+                    case 3:
+                        _queryRange = 3;
+                        break;
+                    case 4:
+                        _queryRange = 5;
+                        break;
+                    case 5:
+                        _queryRange = 10;
+                        break;
+                }
+                SendQuery();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+            }
+        });
     }
 }
